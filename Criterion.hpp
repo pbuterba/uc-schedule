027f7a6e54973867533556726bec6f99265d2043
Preston Buterbaugh <pbuterba.corp@gmail.com>
2024-01-11 22:04:28 -0500

    Add target week field, edit to allow to get first day of week after fixed date

--- Criterion.hpp
+++ Criterion.hpp
@@ -7,22 +7,26 @@ class Criterion {
         bool fixedDay;         /*If true, this criterion checks the location of a specific day. If false, it checks the location of a
                                certain occurrence of a day of the week within the month (e.g. second Tuesday in the month)*/
         int month;             // Month to check (1-12)
-        int day;               // IF fixed date, day within the month to check. ELSE occurrence of the day of the week to check
+        int day;               /* IF fixed date, day within the month to check. ELSE occurrence of the day of the week to check. 0 
+                               for last ocurrence in the month */
         int dayOfWeek;         // 0-6 = Sunday-Saturday
                                // If fixed date, find the first day of this day of week after the specified date
+                               // If set to -1, find the fixed date regardless of day of the week
                                // If day of week occurrence, this is the day of the week to check
         std::string semester;  // Which semester this criterion applies to (Spring, Summer, Fall)
+        int targetWeek;        // The week in which this day would ideally fall, within the given semester
         double weight;         // 1 for most criteria, 0.5 for lower priority
 
     public:
         //Constructor
-        Criterion(std::string name, bool fixedDay, int month, int day, int dayOfWeek, std::string semester, double weight) {
+        Criterion(std::string name, bool fixedDay, int month, int day, int dayOfWeek, std::string semester, int targetWeek, double weight) {
             this->name = name;
             this->fixedDay = fixedDay;
             this->month = month;
             this->day = day;
             this->dayOfWeek = dayOfWeek;
             this->semester = semester;
+            this->targetWeek = targetWeek;
             this->weight = weight;
         }
 
@@ -47,6 +51,9 @@ class Criterion {
         }
         std::string getSemester() {
             return semester;
+        }
+        int getTargetWeek() {
+            return targetWeek;
         }
         double getWeight() {
             return weight;
